39747de9d1eaa26e61607673e748bf8a33e3f8b9
Justin <jyang772@gmail.com>
2015-01-02 00:34:54 -0800

    Create dump.c

---
+++ dump.c
@@ -0,0 +1,29 @@
+//HexDump function
+
+
+void dump(char *buffer, int length){
+    
+    int i,j;
+    char byte;
+    for(i=0; i<length; i++){
+        
+        if(i%16 == 0)
+            printf("%08x ",i);
+        printf("%02x ",buffer[i]);
+        
+        if( (i%16==15) || (i==length-1)){ 
+            for(j=0;j<15-(i%16);j++)              //Fill remaining spaces
+                printf("   "); 
+            printf("| ");
+            
+            for(j=(i-(i%16));j<=i;j++){        //Print out characters. 
+                byte = buffer[j];
+                if(byte > 31 && byte < 127)  //Printable range
+                    printf("%c",byte);
+                else
+                    printf(".");
+            }
+            printf("\n");
+        }
+    }
+}
